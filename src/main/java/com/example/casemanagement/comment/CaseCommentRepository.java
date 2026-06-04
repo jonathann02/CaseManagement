b0c1f347a74e8a4ff666ebd94087d65588e64bd9
Jonathan_Norlin <jonathan.norlin@icloud.com>
2026-06-04 14:37:47 +0200

    Tog bort oanvända comment och audit logg filer.

--- src/main/java/com/example/casemanagement/comment/CaseCommentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.casemanagement.comment;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CaseCommentRepository extends JpaRepository<CaseComment, Long> {
-}
